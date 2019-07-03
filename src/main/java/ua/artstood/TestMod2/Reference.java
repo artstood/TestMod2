4eaee1ff9d6a8f390706f211435927620c9acbf6
artstood <artstood@gmail.com>
2019-07-04 02:56:13 +0300

    Added main class and mod info

---
+++ src/main/java/ua/artstood/TestMod2/Reference.java
@@ -0,0 +1,8 @@
+package ua.artstood.TestMod2;
+
+public class Reference {
+		static final String MODID = "atm2";
+		static final String MODNAME = "Test Mod 2, Yes thats actual name =(";
+		static final String VERSION = "1.0";
+	static final String ACCEPTED_MINECRAFT_VERSIONS ="[1.12.2]";
+}
---
+++ src/main/java/ua/artstood/TestMod2/TestMod.java
@@ -0,0 +1,30 @@
+package ua.artstood.TestMod2;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid=Reference.MODID, name=Reference.MODNAME, version=Reference.VERSION, acceptedMinecraftVersions=Reference.ACCEPTED_MINECRAFT_VERSIONS)
+public class TestMod {
+	@Instance
+	public static TestMod instance;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		System.out.println(Reference.MODID + ":preInit");
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		System.out.println(Reference.MODID + ":init");
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		System.out.println(Reference.MODID + ":postInit");
+	}
+	
+}
